52192c3350c538b9a4cc9e5848b15a2576c1a67d
Ayesha <ayeshamunawarofficial@gmail.com>
2021-05-08 21:09:47 +0500

    Add logo images in svg png and ico formatt and update meanifest.json file

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import './SearchBar.css';
+
+class SearchBar extends Component{
+
+    render() {
+        return(
+            <>
+                <h1>Serach bar</h1>
+            </>
+        )
+    }
+}
+
+export default SearchBar;
