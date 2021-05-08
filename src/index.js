5ce10fd4446b08d5863487d1512291de7b383e36
Ayesha <ayeshamunawarofficial@gmail.com>
2021-05-08 15:31:15 +0500

    refactor code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Card from "./components/Card";
+import Card from "./components/Card/Card";
 import "tachyons";
 
 ReactDOM.render(
