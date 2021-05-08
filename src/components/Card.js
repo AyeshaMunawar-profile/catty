daacfd08b376520b2d52a599c22ddf767783cfd2
Ayesha <ayeshamunawarofficial@gmail.com>
2021-05-08 15:22:32 +0500

    replace the logo with cat logo

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,19 @@ import "./Card.css"
 class Card extends Component {
     render() {
         return (
-            <div className="f1 tc">
-                <h1>{this.props.robotName}</h1>
-                <h4>These the the details related ot the card</h4>
-            </div>
+            <>
+                <div>
+                    <img src='https://robohash.org/1491?set=set4' alt='profile picture of robot'/>
+                    <div>git
+                        <h2>
+                            Mr Mittens
+                        </h2>
+                        <p>AyeshaMunwar@gmail.com</p>
+                    </div>
+                </div>
+            </>
         );
     }
 }
+
 export default Card;
