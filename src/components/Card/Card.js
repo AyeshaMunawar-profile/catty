2d7db333d122b024e19b740512bbccf2527b1807
Ayesha <ayeshamunawarofficial@gmail.com>
2021-05-08 16:42:54 +0500

    Create a crad list component which uses card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,15 +3,19 @@ import "./Card.css"
 
 class Card extends Component {
     render() {
+        const {id, name, username, email} = this.props;
         return (
             <>
-                <div>
-                    <img src='https://robohash.org/1491?set=set4' alt='cat'/>
-                    <div>
-                        <h2>
-                            Mr Mittens
+                <div className='card bg-light-yellow br3 pa4 ma4 grow dib bw2 shadow-5 tc pt5 pb5'>
+                    <img src={`https://robohash.org/${id}?200x200&set=set4`} alt='cat'/>
+                    <div className="card-details">
+                        <h2 className="card-title mt4 mb3 f1">
+                            {name}
                         </h2>
-                        <p>AyeshaMunwar@gmail.com</p>
+                        <h3 className="card-username mb3 f2 normal">
+                            {username}
+                        </h3>
+                        <p className="card-email f3">{email}</p>
                     </div>
                 </div>
             </>
